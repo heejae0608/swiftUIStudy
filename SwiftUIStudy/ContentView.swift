fb9235e4c2b4fdc99b9f5a373c0551b3c02ee43b
heejae0608 <heejae0608@gmail.com>
2023-02-27 15:22:34 +0900

    Initial Commit

---
+++ SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by 히재 on 2023/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
